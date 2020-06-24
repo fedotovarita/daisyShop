0ced7d140ae076d1b567f60673f175fe3234d44c
Rita Fedotova <rita161@rambler.ru>
2020-06-24 22:47:53 +0300

    fix filter issue

--- src/SideBar.js
+++ src/SideBar.js
@@ -1,6 +1,5 @@
 import React from 'react';
 import './SideBar.css';
-import ModalWindowLogin from './ModalWindowLogin';
 import { withRouter } from 'react-router-dom';
 import Products from './Products';
 
@@ -34,123 +33,149 @@ import ProspectPlanter from './prospect planter.jpg';
 import WateringCan from './watering can.jpg';
 import CactiMix from './cacti mix.jpg';
 
-
 const leafy = [
   {
     name: 'monstera',
     image: Monstera,
+    type: 'trending',
   },
   {
     name: 'dieffenbachia',
     image: Dieffenbachia,
+    type: 'patterned',
   },
   {
     name: "calathea ruttlesnake",
     image: Calathea,
+    type: 'patterned',
   },
   {
     name: 'snake plant',
     image: SnakePlant,
+    type: 'patterned',
   },
   {
     name: 'large marble pothos',
     image: LargeMarblePothos,
+    type: 'cascading',
   },
   {
     name: 'philodendron',
     image: Philodendron,
+    type: 'cascading',
   },
   {
     name: 'chameadorea',
     image: Chameadorea,
+    type: 'trending',
   },
   {
     name: 'marble queen pothos',
     image: MarbleQueenPothos,
+    type: 'patterned',
   },
   {
     name: 'faux pothos',
     image: FauxPothos,
+    type: 'cascading',
   },
 ]
 const pots = [
   {
     name: 'balboa planter',
     image: BalboaPlanter,
+    type: 'pots',
   },
   {
     name: 'august planter',
     image: AugustPlanter,
+    type: 'pots',
   },
   {
     name: 'dolores planter',
     image: DoloresPlanter,
+    type: 'pots',
   },
   {
     name: 'olmsted planter',
     image: OlmstedPlanter,
+    type: 'pots',
   },
   {
     name: 'prospect planter',
     image: ProspectPlanter,
+    type: 'pots',
   },
   {
     name: 'fertilizer',
     image: Fertilizer,
+    type: 'accessories',
   },
   {
     name: "potting mix",
     image: PottingMix,
+    type: 'accessories',
   },
   {
     name: 'watering can',
     image: WateringCan,
+    type: 'accessories',
   },
   {
     name: "cacti mix",
     image: CactiMix,
+    type: 'accessories',
   },
 ]
 const succulents = [
   {
     name: 'aloe oik',
     image: AloeOik,
+    type: 'rare',
   },
   {
     name: 'aloe vera',
     image: AloeVera,
+    type: 'succulent',
   },
   {
     name: "crassula",
     image: Crassula,
+    type: 'succulent',
   },
   {
     name: 'gasteria little warty',
     image: GasteriaLittle,
+    type: 'rare',
   },
   {
     name: 'opuntia',
     image: Opuntia,
+    type: 'cacti',
   },
   {
     name: 'mammilaria',
     image: Mammilaria,
+    type: 'cacti',
   },
   {
     name: 'gymnocalycium moon',
     image: GymnocalyciumMoon,
+    type: 'cacti',
   },
   {
     name: "burro's tail",
     image: BurrosTail,
+    type: 'succulent',
   },
   {
     name: 'blue star',
     image: BlueStar,
+    type: 'succulent',
   },
 ]
 
-const leafyFilters = ['patterned', 'trending', 'trailing']
+const leafyFilters = ['patterned', 'trending', 'cascading']
 const succulentsFilters = ['succulent', 'cacti', 'rare']
 const potsFilters = ['pots', 'accessories']
 
@@ -160,45 +185,59 @@ class SideBar extends React.Component {
     this.state = {
       title: '',
       products: [],
+      filteredProducts: [],
       filters: [],
+      category: '',
     }
+    this.handleFilter = this.handleFilter.bind(this);
   };
 
-  static getDerivedStateFromProps (nextProps) {
-    let category = nextProps.match.params.category;
+  static getDerivedStateFromProps (nextProps, prevState) {
+    console.log('New Props received', nextProps)
 
-    if (category == 'leafy_plants'){
-      return {
-        title: 'leafy plants',
-        products: leafy,
-        filters: leafyFilters,
-      };
-    } else if (category == 'succulents'){
-      return {
-        title: 'succulents',
-        products: succulents,
-        filters: succulentsFilters,
-      };
-    } else if (category == 'pots_accessories'){
-      return {
-        title: 'pots & accessories',
-        products: pots,
-        filters: potsFilters,
-      };
-    } else {
-      return null;
+    let category = nextProps.match.params.category;
+    let newState = {};
+
+    if (category === 'leafy_plants') {
+      newState.title = 'leafy plants';
+      newState.products = leafy;
+      newState.filters = leafyFilters;
+    } else if (category === 'succulents') {
+      newState.title = 'succulents';
+      newState.products = succulents;
+      newState.filters = succulentsFilters;
+    } else if (category === 'pots_accessories') {
+      newState.title = 'pots & accessories';
+      newState.products = pots;
+      newState.filters = potsFilters;
     }
+    newState.category = category;
+    if (prevState.category != category) {
+      newState.filteredProducts = [];
+    }
+
+    return newState;
   }
 
-  preventHandler (event) {
-    event.preventDefault();
-
+  handleFilter (filter) {
+    var result = this.state.products.filter((item) => item.type === filter);
+    this.setState({filteredProducts: result});
   }
 
   render () {
-    const filters = this.state.filters.map((filter, index) => {
-      return <li key={index}><a onClick={this.preventHandler.bind(this)}>{filter}</a></li>
+    const filters = this.state.filters.map((item, index) => {
+      return (
+        <li key={index}>
+          <a onClick={() => this.handleFilter(item)}>
+            {item}
+          </a>
+        </li>
+      );
     })
+
+    const products = (this.state.filteredProducts.length != 0)
+      ? this.state.filteredProducts
+      : this.state.products;
 
     return <div className="section">
       <div className="columns">
@@ -213,7 +252,7 @@ class SideBar extends React.Component {
           </aside>
         </div>
         <Products
-          products={this.state.products}
+          products={products}
           title={this.state.title}
         />
         <div className="column is-2">
